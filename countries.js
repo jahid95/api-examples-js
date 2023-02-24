70c90903f36bef3036dad5f4e746a133cd9f4720
Anwar <anwarjahid449@gmail.com>
2023-02-24 14:50:10 +0600

    countries api

---
+++ countries.js
@@ -0,0 +1,22 @@
+function loadCountries(){
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayCountries(data))
+}
+
+function  displayCountries(data){
+    const container = document.getElementById('container');
+    for(const i of data){
+        console.log(i);
+        const li = document.createElement('li');
+        li.classList.add('country');
+        li.innerHTML = `<img src=${i.flags.svg}>
+        <p>${i.name.official}</p>
+        <p>${i.capital ? i.capital[0] : 'No Capital %'}</p>
+        `;
+        container.appendChild(li);
+       
+    }
+}
+
+loadCountries();
